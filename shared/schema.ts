dbff9c9ea897ffa1de8a01120fbd1cbe8dc6c849
paritkansal121 <paritkansal121@gmail.com>
2026-05-18 08:11:25 +0530

    refactor: move description markdown rendering from experience level to individual project level

--- shared/schema.ts
+++ shared/schema.ts
@@ -130,7 +130,6 @@ export const experience = sqliteTable("experience", {
   role: text("role").notNull(),
   period: text("period").notNull(),
   location: text("location").notNull(),
-  description: text("description"),
   projects: text("projects", { mode: "json" }).$type<{ title: string, description: string, tags: string[] }[]>().notNull(),
 });
 
